2c670d51f72e4513ba4c166a6f8734616e75b19d
Tejprakashsing <122627826+Tejprakashsing@users.noreply.github.com>
2025-06-16 10:53:09 +0530

    {Tej} Add Q8_Multiplication6to9.java

--- Arrays - practice sheet - 1/Q8_Multiplication6to9.java
+++ Arrays - practice sheet - 1/Q8_Multiplication6to9.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Q8_Multiplication6to9 {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int number = sc.nextInt();
