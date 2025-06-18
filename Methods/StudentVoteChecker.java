d7c80aa5abe011de6d9cb04b2427ba33f88fa39f
Tejprakashsing <122627826+Tejprakashsing@users.noreply.github.com>
2025-06-18 08:56:11 +0530

    {Tej} Add StudentVoteChecker.java

--- Methods/StudentVoteChecker.java
+++ Methods/StudentVoteChecker.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class StudentVoteChecker {
+public class StudentVoteChecker { 
     public static boolean canStudentVote(int age) {
         if (age < 0) return false;
         return age >= 18;
