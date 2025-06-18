5d6f66124dc548471f8ca3193f0c577ac88efaae
Tejprakashsing <122627826+Tejprakashsing@users.noreply.github.com>
2025-06-18 08:59:35 +0530

    {Tej} Add WindChill.java

--- Methods/WindChill.java
+++ Methods/WindChill.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class WindChill {
     public static double calculateWindChill(double temperature, double windSpeed) {
-        return 35.74 + 0.6215 * temperature + (0.4275 * temperature - 35.75) * windSpeed;
+        return 35.74 + 0.6215 * temperature + (0.4275 * temperature - 35.75) * windSpeed; 
     }
 
     public static void main(String[] args) {
