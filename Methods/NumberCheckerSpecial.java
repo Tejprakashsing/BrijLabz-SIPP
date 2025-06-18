1b694724bd182055355736cc7a659c56253a0bfb
Tejprakashsing <122627826+Tejprakashsing@users.noreply.github.com>
2025-06-18 08:49:37 +0530

    {Tej} Add NumberCheckerSpecial.java

--- Methods/NumberCheckerSpecial.java
+++ Methods/NumberCheckerSpecial.java
@@ -4,7 +4,7 @@ public class NumberCheckerSpecial {
     public static boolean isPrime(int number) {
         if (number <= 1) return false;
         for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i == 0) return false;
+            if (number % i == 0) return false; 
         }
         return true;
     }
