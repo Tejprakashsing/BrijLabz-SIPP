cac664eea648c084d4a7710af0800656cdb3b244
Tejprakashsing <122627826+Tejprakashsing@users.noreply.github.com>
2025-06-18 08:59:09 +0530

    {Tej} Add UnitConverterTemperature.java

--- Methods/UnitConverterTemperature.java
+++ Methods/UnitConverterTemperature.java
@@ -4,7 +4,7 @@ public class UnitConverterTemperature {
     public static double convertFahrenheitToCelsius(double fahrenheit) {
         return (fahrenheit - 32) * 5 / 9;
     }
-
+ 
     public static double convertCelsiusToFahrenheit(double celsius) {
         return (celsius * 9 / 5) + 32;
     }
