5613a9cf55128b23a5db6ac982e01faa7b6d19ee
Tejprakashsing <122627826+Tejprakashsing@users.noreply.github.com>
2025-06-18 08:55:29 +0530

    {Tej} Add SpringSeason.java

--- Methods/SpringSeason.java
+++ Methods/SpringSeason.java
@@ -9,6 +9,6 @@ public class SpringSeason {
     public static void main(String[] args) {
         int month = Integer.parseInt(args[0]);
         int day = Integer.parseInt(args[1]);
-        System.out.println(isSpringSeason(month, day) ? "It's a Spring Season" : "Not a Spring Season");
+        System.out.println(isSpringSeason(month, day) ? "It's a Spring Season" : "Not a Spring Season"); 
     }
 }
