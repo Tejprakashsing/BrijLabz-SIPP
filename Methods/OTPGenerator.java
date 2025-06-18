ee6fdff1b707cb2432e5ca0cd78b1f729693a7a9
Tejprakashsing <122627826+Tejprakashsing@users.noreply.github.com>
2025-06-18 08:53:22 +0530

    {Tej} Add OTPGenerator.java

--- Methods/OTPGenerator.java
+++ Methods/OTPGenerator.java
@@ -2,7 +2,7 @@ import java.util.HashSet;
 
 public class OTPGenerator {
     public static int generateOTP() {
-        return (int) (Math.random() * 900000 + 100000);
+        return (int) (Math.random() * 900000 + 100000); 
     }
 
     public static boolean areUnique(int[] otps) {
