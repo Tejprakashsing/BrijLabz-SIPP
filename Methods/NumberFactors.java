31f3c45d185bf7233c1ebdc140af63c38fac2d0c
Tejprakashsing <122627826+Tejprakashsing@users.noreply.github.com>
2025-06-18 08:50:48 +0530

    {Tej} Add NumberFactors.java

--- Methods/NumberFactors.java
+++ Methods/NumberFactors.java
@@ -9,7 +9,7 @@ public class NumberFactors {
         int[] factors = new int[count];
         int index = 0;
         for (int i = 1; i <= number; i++) {
-            if (number % i == 0) factors[index++] = i;
+            if (number % i == 0) factors[index++] = i; 
         }
         return factors;
     }
