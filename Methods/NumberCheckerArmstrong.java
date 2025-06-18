97cc7c05b569bac5719017b83631c6f68f50ef75
Tejprakashsing <122627826+Tejprakashsing@users.noreply.github.com>
2025-06-18 08:48:25 +0530

    {Tej} Add NumberCheckerArmstrong.java

--- Methods/NumberCheckerArmstrong.java
+++ Methods/NumberCheckerArmstrong.java
@@ -10,7 +10,7 @@ public class NumberCheckerArmstrong {
         int[] digits = new int[numStr.length()];
         for (int i = 0; i < numStr.length(); i++) {
             digits[i] = numStr.charAt(i) - '0';
-        }
+        } 
         return digits;
     }
 
