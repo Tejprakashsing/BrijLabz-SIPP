e59a080cd737654fe3e26a3448aad6a7528c7db5
Tejprakashsing <122627826+Tejprakashsing@users.noreply.github.com>
2025-06-18 08:45:50 +0530

    {Tej} Add LeapYear.java

--- Methods/LeapYear.java
+++ Methods/LeapYear.java
@@ -4,7 +4,7 @@ public class LeapYear {
     public static boolean isLeapYear(int year) {
         if (year < 1582) return false;
         return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-    }
+    } 
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
